ef64d4b47b460181f3caa2267a47d7cebff56b83
Majoi <128968753+0xforwh4t@users.noreply.github.com>
2026-04-11 00:55:32 +0630

    Update wacky.py

--- HTB/WingData/wacky.py
+++ HTB/WingData/wacky.py
@@ -1,1 +1,12 @@
 
+import hashlib
+
+target = "32940de----------------------------------f8a503ca"
+
+with open("/usr/share/wordlists/rockyou.txt", "r", errors="ignore") as f:
+    for line in f:
+        pw = line.strip()
+        h = hashlib.sha256((pw + "WingFTP").encode()).hexdigest()
+        if h == target:
+            print("FOUND:", pw)
+            break
